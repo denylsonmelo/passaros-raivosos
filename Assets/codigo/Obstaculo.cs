3de3da7ff3319828c28458ec81a08930211d7186
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2023-06-07 23:32:08 -0300

    modificando componentes na cena com scripts

--- Assets/codigo/Obstaculo.cs
+++ Assets/codigo/Obstaculo.cs
@@ -4,29 +4,61 @@ using UnityEngine;
 
 public class Obstaculo : MonoBehaviour
 {
+    [SerializeField]
+    int vida = 50;
+    [SerializeField]
+    Sprite[] imagens;
+
+    private SpriteRenderer imagemAtual;
+    [SerializeField]
+    private int vidaAtual;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("executou uma vez");
 
+        vidaAtual = vida;
+
+        imagemAtual = GetComponent<SpriteRenderer>();
+        imagemAtual.sprite = imagens[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("executando a cada quadro");
+        //Debug.Log("executando a cada quadro");
+        verificarVida();
+    }
+
+    void verificarVida(){
+        if(vidaAtual <= (vida * 0.7)){
+            imagemAtual.sprite = imagens[1];
+        }
+        if(vidaAtual <= (vida * 0.3)){
+            imagemAtual.sprite = imagens[2];
+        }
+        if(vidaAtual <= 0 ){
+            Debug.Log("bloco morreu");
+            vidaAtual = 0;
+            Destroy(this.gameObject);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D batida)
     {
         if(batida.gameObject.tag == "Player"){
             Debug.Log("batida no jogador");
+            vidaAtual = vidaAtual - 12;
         }
         if(batida.gameObject.tag == "chao"){
             Debug.Log("batida no chao");
+            vidaAtual = vidaAtual - 5;
         }
         if(batida.gameObject.tag == "obstaculo"){
             Debug.Log("batida no obstaculo");
+            vidaAtual = vidaAtual - 8;
+
         }
         
     }
